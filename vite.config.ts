e3dfd6f55e5e0b604938d94487b9813abae5f6d2
tanishsain <tanishsain2407@gmail.com>
2026-05-28 11:49:39 +0530

    revert vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,3 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
-})
+export default defineConfig();
